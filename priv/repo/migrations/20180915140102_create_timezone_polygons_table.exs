7bac2604e6f823b7722fd867fc393e96dfb5291c
michael.k.pfeifer@googlemail.com <mick@home.mick.net>
2018-09-16 11:43:14 +0200

    Add migration for the table holding the timezone polygons

---
+++ priv/repo/migrations/20180915140102_create_timezone_polygons_table.exs
@@ -0,0 +1,16 @@
+defmodule TZS.Repo.Migrations.CreateTimezonePolygonsTable do
+  use Ecto.Migration
+
+  def up do
+    create table(:timezone_polygons, primary_key: false) do
+      add(:gid, :serial, primary_key: true)
+      add(:tzid, :string, size: 80)
+    end
+    execute("SELECT AddGeometryColumn ('timezone_polygons', 'polygon', 4326, 'MULTIPOLYGON', 2);")
+    create(index(:timezone_polygons, [:polygon], using: :gist))
+  end
+
+  def down do
+    drop table(:timezone_polygons)
+  end
+end
